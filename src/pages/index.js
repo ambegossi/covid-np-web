13cc9a4add63ec60a0c8790aa45fff86c3615199
Anderson Begossi <andersonbegossi@gmail.com>
2020-05-15 18:26:04 -0300

    refactored page home

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import Home from './Home';
+import Home from '../components/Home';
 
 const IndexPage = () => (
   <Layout>
