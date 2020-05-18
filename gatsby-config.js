8061adf7ec645c0351500a65792d8371c49dbc60
Anderson Begossi <andersonbegossi@gmail.com>
2020-05-18 16:13:08 -0300

    add graphql query to index

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,21 @@ module.exports = {
         icon: `src/images/logo-np.svg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    'gatsby-transformer-remark',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { graphql } from 'gatsby';
+
 import flag from "../images/flag.svg";
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
@@ -16,45 +18,64 @@ import {
   FlagDescription,
 } from '../styles/index/styles.js';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Início" lang="pt" />
-        <Container>
-      <Cases>
-        <CaseItem>
-          <CaseNumber>17</CaseNumber>
-          <CaseText>Casos confirmados</CaseText>
-        </CaseItem>
-        <CaseItem>
-          <CaseNumber>08</CaseNumber>
-          <CaseText>Casos curados</CaseText>
-        </CaseItem>
-        <CaseItem>
-          <CaseNumber>03</CaseNumber>
-          <CaseText>Casos em investigação</CaseText>
-        </CaseItem>
-        <CaseItem>
-          <CaseNumber>58</CaseNumber>
-          <CaseText>Casos descartados</CaseText>
-        </CaseItem>
-      </Cases>
-      <Flag>
-        <img src={flag} alt="Bandeira de distanciamento"/>
-        <FlagContent>
-          <FlagTitle>Bandeira laranja - Risco médio</FlagTitle>
-          <FlagDescription>A região encontra-se em um dos dois cenários: 1- Média capacidade do sistema de saúde e 
-            baixa propagação do vírus ou 2- Alta capacidade do sistema de saúde e média propagação do vírus.
-          </FlagDescription>
-        </FlagContent>
-      </Flag>
-      
-      <a href="http://novaprata.rs.gov.br/">Decreto 8085/2020</a>
+const IndexPage = (props) => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
 
-      <p>Proteja-se!</p>
-      <p>#UseMáscara</p>
-    </Container>
+  return (
+    <Layout>
+      <SEO title="Início" lang="pt" />
+          <Container>
+        <Cases>
+          <CaseItem>
+            <CaseNumber>{data.confirmed}</CaseNumber>
+            <CaseText>Casos confirmados</CaseText>
+          </CaseItem>
+          <CaseItem>
+            <CaseNumber>08</CaseNumber>
+            <CaseText>Casos curados</CaseText>
+          </CaseItem>
+          <CaseItem>
+            <CaseNumber>03</CaseNumber>
+            <CaseText>Casos em investigação</CaseText>
+          </CaseItem>
+          <CaseItem>
+            <CaseNumber>58</CaseNumber>
+            <CaseText>Casos descartados</CaseText>
+          </CaseItem>
+        </Cases>
+        <Flag>
+          <img src={flag} alt="Bandeira de distanciamento"/>
+          <FlagContent>
+            <FlagTitle>Bandeira laranja - Risco médio</FlagTitle>
+            <FlagDescription>A região encontra-se em um dos dois cenários: 1- Média capacidade do sistema de saúde e 
+              baixa propagação do vírus ou 2- Alta capacidade do sistema de saúde e média propagação do vírus.
+            </FlagDescription>
+          </FlagContent>
+        </Flag>
+        
+        <a href="http://novaprata.rs.gov.br/">Decreto 8085/2020</a>
 
-  </Layout>
-)
+        <p>Proteja-se!</p>
+        <p>#UseMáscara</p>
+      </Container>
+
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allFile (filter: {sourceInstanceName: {eq: "content"} name: {eq: "home"}}) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              confirmed
+          }
+        }
+      }
+    }
+  }
+}`
